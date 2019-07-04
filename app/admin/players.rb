41ab20f8c418d1d79a2372f1194a2d2d59e1d5ec
AlexBahlk <a.bahlk@klara.com>
2019-07-04 11:20:34 +0200

    add edit player

--- app/admin/players.rb
+++ app/admin/players.rb
@@ -4,13 +4,25 @@ include ActiveAdmin::ViewsHelper
 ActiveAdmin.register Player do
   menu label: 'Player'
 
-  actions :index
+  actions :index, :show, :edit, :update, :destroy
   scope :all
+  permit_params :player_name, :color
 
   index do
     selectable_column
     id_column
     column :player_name
     column :created_at
+    column :world
+    actions
   end
+
+  form do |f|
+    f.inputs 'Player' do
+      f.input :player_name
+      f.input :color
+    end
+    f.actions
+  end
+
 end
